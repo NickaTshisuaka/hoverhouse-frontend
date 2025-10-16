21b12dfb4a0796127a05d80498c00bd90c7b2ef4
Niclette Tshisuaka <nicka20060108@gmail.com>
2025-10-16 13:35:02 +0200

    fixed the spelling of dashboard css

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./Dashboard.css";
+import "./dashboard.css";
 
 const API = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
